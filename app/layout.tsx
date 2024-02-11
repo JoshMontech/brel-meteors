13c9622e6cb806cd8f4cf75e0d1a40beda5586b5
Josh <JoshMontech@users.noreply.github.com>
2024-02-10 21:52:46 -0600

    removed themeing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css"
-import Providers from "./providers"
 import MainHeader from "@/components/MainHeader"
 
 export default function RootLayout({
@@ -10,10 +9,8 @@ export default function RootLayout({
     return (
         <html lang="en" className="flex h-full w-full">
             <body className="relative flex h-full w-full flex-col">
-                <Providers>
-                    <MainHeader />
-                    {children}
-                </Providers>
+                <MainHeader />
+                {children}
             </body>
         </html>
     )
